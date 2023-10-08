0ca97e0893f4f56b2a26fe3293f87606241f608f
missanorajjohnthattil <missanorajjohnthattil@gmail.com>
2023-10-08 09:42:47 +0530

    phase 1 with error

--- assignments/students_profile/main.go
+++ assignments/students_profile/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+//import "strings"
+import "fmt"
 
+func main() {
+	studentname := "default"
+	studentid := 1000
+	// studentaddress := make(map[string]string)
+	phone_number := 9876543210
+	//magiccode := luckcodegen(phone_number,studentid)
+
+	/*func deletestudent(studentid,magiccode){
+
+	  }
+	*/
+
+	/*func getstudent(var studentid) string{
+	  	fmt.printf(" id: %d \n name: %s \n phone no: %d \n code: %d", studentid, studentname, phone_number, magiccode)
+	  	for(i,address := range studentstudentaddress ){
+	  		fmt.println(i,") ",address)
+	  	}
+	  }
+
+	  func luckcodegen(phone_number, key := studentid) int{
+	  	codearray := make([10]int,10,10)
+	  	codearray = phone_number
+	  return codearray%key
+	  }
+	*/
+	  test()
+	
+	func test() string{
+		code := 1000
+		fmt.Printf(" id : %d \n name : %s \n ph_no : %d \n %d \n", studentid, studentname, phone_number, code)
+		return "completed"
+	}
 }
