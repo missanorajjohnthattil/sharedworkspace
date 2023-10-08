4a5d88264644895b9d9f64f15b5f745b5fb130c6
missanorajjohnthattil <missanorajjohnthattil@gmail.com>
2023-10-08 12:00:51 +0530

    phase 1

--- assignments/students_profile/main.go
+++ assignments/students_profile/main.go
@@ -1,36 +1,80 @@
 package main
 
-import "strings"
-import "fmt"
+import . "fmt"
 
-var Studentname = "default"
-var Studentid = 1000
+// -------------------------------------- defining global variables here------------------------------
 
-var Studentaddress = make(map[string]string)
-var Phone_number = 9876543210
+var studentList = make(map[int]student)
 
-var Magiccode = luckcodegen(phone_number,studentid)
+type student struct {
+	name        string
+	phoneNumber string
+}
 
+var studentID int
+
+// ----------------------------------- Main function -------------------------
 func main() {
+	for {
+		Println("Welcome to student register")
+		Println(`
+		1: Create student
+		2: View Student details
+		3: Delete student
+		4: Get magic code
+		0: Exit
+		`)
+		var option int
+		Scanf("%d", &option)
 
-
-}
-
-func deletestudent(Studentid,Magiccode){
-
-}
-
-
-func getstudent(studentid) string{
-	fmt.printf(" id: %d \n name: %s \n phone no: %d \n code: %d", studentid, studentname, phone_number, magiccode)
-	for(int i = 1,address := range Studentaddress){
-	fmt.println(i,")",address)
+		switch option {
+		case 0:
+			return
+		case 1:
+			createStudent()
+		case 2:
+			viewStudent()
+		default:
+			Printf("invalid option : %d \n", option)
+		}
 	}
 }
 
-func luckcodegen(phone_number, key := studentid) int{
-	codearray := make([10]int,10,10)
-	codearray = phone_number
-	return codearray%key
+// -------------------------- create strudent function ---------------------
+func createStudent() {
+	var name, confirmation, phoneNumber string
+
+	Println("Fill up the details to create the student")
+	Printf("Enter your name : ")
+	Scanf("%s", &name)
+	Printf("\nEnter your phone number : ")
+	Scanf("%s", &phoneNumber)
+
+	Printf(`We are creating your student id with follwing details:
+		Name : %s
+		Phone Number : %s
+		
+		For confirmation press y :
+		`, name, phoneNumber)
+	Scanf("%s", &confirmation)
+	switch confirmation {
+	case "y":
+		newstudent := student{
+			name:        name,
+			phoneNumber: phoneNumber,
+		}
+		studentID++
+		studentList[studentID] = newstudent
+		Printf("New student ID of %d is registered \n", studentID)
+	default:
+		Println("Aborting the student creation...")
+	}
 }
-	
+
+// -------------------------- view strudent function ---------------------
+func viewStudent() {
+	var studentID int
+	Printf("Enter the student id to view : ")
+	Scanf("%d", studentID)
+	Printf("%+v", studentList[studentID])
+}
