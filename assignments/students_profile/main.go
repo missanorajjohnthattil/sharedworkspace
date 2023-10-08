460312a21ce9d33ea0d534124743557fb7dd069e
missanorajjohnthattil <missanorajjohnthattil@gmail.com>
2023-10-08 10:07:53 +0530

    phase 1 test2

--- assignments/students_profile/main.go
+++ assignments/students_profile/main.go
@@ -1,40 +1,36 @@
 package main
 
-//import "strings"
+import "strings"
 import "fmt"
 
 var Studentname = "default"
 var Studentid = 1000
 
-// Studentaddress := make(map[string]string)
+var Studentaddress = make(map[string]string)
 var Phone_number = 9876543210
 
-//Sagiccode := luckcodegen(phone_number,studentid)
+var Magiccode = luckcodegen(phone_number,studentid)
 
 func main() {
 
-	/*func deletestudent(studentid,magiccode){
 
-	  }
-	*/
-
-	/*func getstudent(var studentid) string{
-	  	fmt.printf(" id: %d \n name: %s \n phone no: %d \n code: %d", studentid, studentname, phone_number, magiccode)
-	  	for(i,address := range studentstudentaddress ){
-	  		fmt.println(i,") ",address)
-	  	}
-	  }
-
-	  func luckcodegen(phone_number, key := studentid) int{
-	  	codearray := make([10]int,10,10)
-	  	codearray = phone_number
-	  return codearray%key
-	  }
-	*/
-	test()
 }
-func test() string {
-	code := 1000
-	fmt.Printf(" id : %d \n name : %s \n ph_no : %d \n %d \n", Studentid, Studentname, Phone_number, code)
-	return "completed"
+
+func deletestudent(Studentid,Magiccode){
+
 }
+
+
+func getstudent(studentid) string{
+	fmt.printf(" id: %d \n name: %s \n phone no: %d \n code: %d", studentid, studentname, phone_number, magiccode)
+	for(int i = 1,address := range Studentaddress){
+	fmt.println(i,")",address)
+	}
+}
+
+func luckcodegen(phone_number, key := studentid) int{
+	codearray := make([10]int,10,10)
+	codearray = phone_number
+	return codearray%key
+}
+	
